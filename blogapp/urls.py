640bb8f125cfbeef90b4a6842d11cd9c3363cfc7
Raj Bansal <rajbansal.hello@gmail.com>
2022-04-21 13:09:17 +0530

    Added request to delete post

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('add-post', views.Addpost, name = 'Addpost'),
-    path('post/<str:pk>', views.post, name = "post")
+    path('post/<str:pk>', views.post, name = "post"),
+    path('delete-post/<str:pk>', views.Deletepost, name = "Deletepost")
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from pyexpat import model
+from turtle import pos
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
@@ -19,8 +21,19 @@ def Addpost(request):
     newPost.body = jsonData['body']
     newPost.save()
     return HttpResponse("success")
+
 def post(request, pk):
     post = Post.objects.get(id = pk)
     return render(request, 'post.html', {'post':post})
+
+@csrf_exempt
+def Deletepost(request, pk):
+    try:
+        post = Post.objects.get(id = pk)
+        post.delete()
+        return HttpResponse("successfully deleted!")
+    except Post.DoesNotExist:
+        return HttpResponseBadRequest("Object does not exist")
+
 
 
