9b6696d653ddb426af628c9561af4d7e74a1baf6
Raj Bansal <rajbansal.hello@gmail.com>
2022-04-21 11:54:37 +0530

    Added http request to add post

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts':posts})
+
+@csrf_exempt
+def Addpost(request):
+    jsonData = JSONParser().parse(request)
+    print(jsonData)
+    newPost = Post()
+    newPost.title = jsonData['title']
+    newPost.body = jsonData['body']
+    newPost.save()
+    return HttpResponse("success")
